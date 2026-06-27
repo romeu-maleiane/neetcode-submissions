3cdbb2d0187754b2547d185083a8ac53368737d5
Romeu Maleiane <romeumaleiane1@gmail.com>
2026-06-27 22:40:53 +0200

    Add: frequency-of-the-most-frequent-element - submission-0

---
+++ Data Structures & Algorithms/frequency-of-the-most-frequent-element/submission-0.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number}
+     */
+    maxFrequency(nums, k) {
+        nums.sort((a,b) => a - b)
+
+        let l = 0; let r = 0; let res = 0; let total = 0
+
+        while ( r < nums.length) {
+            total += nums[r]
+
+            while(nums[r] * (r - l + 1) > total + k){
+                total -= nums[l]
+                l += 1
+            }
+            res = Math.max(res, (r - l + 1))
+            r += 1
+        }
+
+        return res
+    }
+}
