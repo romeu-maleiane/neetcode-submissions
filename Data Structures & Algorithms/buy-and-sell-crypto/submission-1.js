d0b4c9e4ba28abe9ce07dc82e04510376dfac017
Romeu Maleiane <romeumaleiane1@gmail.com>
2026-09-06 00:41:17 +0200

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let maxP = 0
+        let l = 0;let r = 0
+
+        while(r < prices.length){
+            if(prices[l] < prices[r]){
+                let profit = prices[r] - prices[l]
+                maxP = Math.max(profit, maxP)
+            }
+            else l = r
+            r++
+        }
+
+        return maxP
+    }
+}
